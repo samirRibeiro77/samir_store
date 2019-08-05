8beb33d9caf454e050c4828de344a1a75b25bbba
Samir Ribeiro <samir.sjr77@gmail.com>
2019-08-04 23:08:39 -0300

    Grid and List view.

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ProductData {
   String _id, _title, _description, _category;
   double _price;
-  List<String> _images, _sizes;
+  List _images, _sizes;
 
   ProductData.fromDocument(DocumentSnapshot doc) {
     this._id = doc.documentID;
@@ -13,4 +13,12 @@ class ProductData {
     this._images = doc.data["images"];
     this._sizes = doc.data["sizes"];
   }
+
+  List get sizes => _sizes;
+  List get images => _images;
+  double get price => _price;
+  String get category => _category;
+  String get description => _description;
+  String get title => _title;
+  String get id => _id;
 }
--- lib/ui/widget/tile/product_tile.dart
+++ lib/ui/widget/tile/product_tile.dart
@@ -1,14 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:samir_store/data/product_data.dart';
+import 'package:samir_store/ui/screen/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String _type;
-  final ProductData _data;
+  final ProductData _product;
 
-  ProductTile(this._type, this._data);
+  ProductTile(this._type, this._product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ProductScreen(_product))
+        );
+      },
+      child: Card(
+        child: _type == "grid"
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      _product.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            _product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$ ${_product.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            : Row(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                _product.images[0],
+                fit: BoxFit.cover,
+                height: 250.0,
+              )
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _product.title,
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "R\$ ${_product.price.toStringAsFixed(2)}",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
